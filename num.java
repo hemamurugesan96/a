0999b64eb0b793d42b11a6b1fdf1f412cba9e3e5
hemamurugesan96 <hemamurugesanece@gmail.com>
2017-06-14 14:27:05 +0530

    num.java

---
+++ num.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class num
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+if(n>0)
+{
+System.out.println("positive num");
+}
+else if(n<0)
+{
+System.out.println("negative num");
+}
+else 
+{
+System.out.println("zero");
+}
+}
+}
